4c0f3656e1cdaee38991be130c28dcd5f842d727
Daniel Locke <locke.dj@hotmail.com>
2018-12-29 18:58:50 +1000

    Style tag tokenizer

--- html-tokenizer.go
+++ html-tokenizer.go
@@ -56,6 +56,7 @@ func newReader(text []byte) *reader {
 type Portions struct {
 	Positions [][2]int 
 	Bolded, Italicised []bool
+	Adjusted [][2]int
 }
 
 // Need to change this to be depth based for bolding and italics ... 
@@ -122,7 +123,164 @@ func TextPos(text []byte) (Portions, error) {
 			inds, 
 			bolded,
 			italics,
+			nil,
 		}, nil 
+}
+
+func TextPosWithCleanStyleTags(text []byte) (Portions, []byte, error) {
+	reader := newReader(text)
+	tokenizer := html.NewTokenizer(reader)
+
+	prev := 0
+	var boldDepth, italicDepth, adjustedPos int
+
+	inds := make([][2]int, 0)
+	adjusted := make([][2]int, 0)
+	italics := make([]bool, 0)
+	bolded := make([]bool, 0)
+
+	var outBuff bytes.Buffer
+
+loop:
+	for {
+		token := tokenizer.Next()
+		switch token {
+		case html.ErrorToken:
+			if tokenizer.Err() == io.EOF {
+				break loop
+			} else {
+				return Portions{}, nil, fmt.Errorf("Error: %s\n", html.ErrorToken)
+			}
+		case html.TextToken:
+			outBuff.Write(tokenizer.Raw())
+			l := len(tokenizer.Raw())
+			inds = append(inds, [2]int{prev, prev+l})
+			italicised := false
+			if italicDepth > 0 {
+				italicised = true
+			}
+			italics = append(italics, italicised)
+			bold := false
+			if boldDepth > 0 {
+				bold = true
+			}
+			bolded = append(bolded, bold)
+			adjusted= append(adjusted, [2]int{adjustedPos, adjustedPos+l})
+			adjustedPos += l
+		case html.EndTagToken:
+			prev = int(reader.pos())
+			tag, _ := tokenizer.TagName()
+			if bytes.Equal(tag, []byte("i")) {
+				italicDepth--
+				if italicDepth < 0 {
+					return Portions{}, nil, fmt.Errorf("Malformed html with italic depth below 0.\n")
+				}
+				// for </b>
+				adjustedPos += 4
+				outBuff.WriteString("</i>")
+			} else if bytes.Equal(tag, []byte("b")) {
+				boldDepth--
+				if boldDepth < 0 {
+					return Portions{}, nil, fmt.Errorf("Malformed html with bold depth below 0.\n")
+				}
+				adjustedPos += 4
+				outBuff.WriteString("</b>")
+			}
+		case html.StartTagToken:
+			prev = int(reader.pos())
+			tag, _ := tokenizer.TagName()
+			if bytes.Equal(tag, []byte("i")) {
+				italicDepth++
+				adjustedPos += 3
+				outBuff.WriteString("<i>")
+			} else if bytes.Equal(tag, []byte("b")) {
+				boldDepth++
+				adjustedPos += 4
+				outBuff.WriteString("<b>")
+			}
+		case html.SelfClosingTagToken:
+			prev = int(reader.pos())
+			tag, _ := tokenizer.TagName()
+			if bytes.Equal(tag, []byte("br")) {
+				outBuff.WriteByte(' ')
+				adjustedPos += 1
+			}
+		default:
+			prev = int(reader.pos())
+		}
+	}
+
+	return Portions{
+		inds,
+		bolded,
+		italics,
+		adjusted,
+	}, outBuff.Bytes(), nil
+}
+
+
+func CleanText(text []byte) ([]byte, error) {
+	r := bytes.NewReader(text)
+	tokenizer := html.NewTokenizer(r)
+
+	var outBuff bytes.Buffer
+
+loop:
+	for {
+		token := tokenizer.Next()
+		switch token {
+		case html.ErrorToken:
+			if tokenizer.Err() == io.EOF {
+				break loop
+			} else {
+				return nil, fmt.Errorf("Error: %s\n", html.ErrorToken)
+			}
+		case html.TextToken:
+			outBuff.Write(tokenizer.Raw())
+		}
+	}
+	return outBuff.Bytes(), nil
+}
+
+func CleanTextWithStyleTags(text []byte) ([]byte, error) {
+	r := bytes.NewReader(text)
+	tokenizer := html.NewTokenizer(r)
+
+	var outBuff bytes.Buffer
+loop:
+	for {
+		token := tokenizer.Next()
+		switch token {
+		case html.ErrorToken:
+			if tokenizer.Err() == io.EOF {
+				break loop
+			} else {
+				return nil, fmt.Errorf("Error: %s\n", html.ErrorToken)
+			}
+		case html.TextToken:
+			outBuff.Write(tokenizer.Raw())
+		case html.EndTagToken:
+			tag, _ := tokenizer.TagName()
+			if bytes.Equal(tag, []byte("i")) {
+				outBuff.WriteString("</i>")
+			} else if bytes.Equal(tag, []byte("b")) {
+				outBuff.WriteString("</b>")
+			}
+		case html.StartTagToken:
+			tag, _ := tokenizer.TagName()
+			if bytes.Equal(tag, []byte("i")) {
+				outBuff.WriteString("<i>")
+			} else if bytes.Equal(tag, []byte("b")) {
+				outBuff.WriteString("<b>")
+			}
+		case html.SelfClosingTagToken:
+			tag, _ := tokenizer.TagName()
+			if bytes.Equal(tag, []byte("br")) {
+				outBuff.WriteByte(' ')
+			}
+		}
+	}
+	return outBuff.Bytes(), nil
 }
 
 // func test(text string) {
